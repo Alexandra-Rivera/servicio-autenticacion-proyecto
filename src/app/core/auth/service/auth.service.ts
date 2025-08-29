eab2029bca17e18f7032e8908f2cdb686637a4ca
Alexandra Rivera <00162121@uca.edu.sv>
2025-08-29 16:28:03 -0600

    Modificando auth.service

--- src/app/core/auth/service/auth.service.ts
+++ src/app/core/auth/service/auth.service.ts
@@ -11,18 +11,4 @@ export class AuthService {
   private SERVER_URL = environment.SERVER_URL;
 
   constructor(private http: HttpClient, private router: Router) { }
-
-
-  /*Login de usuario*/
-  login(login_info: Login): Observable<any> {
-    return this.http.post(`${this.server_url}/auth/login`, login_info).pipe(
-      tap((response: any) => {
-        if (response && response.token) {
-            this.setToken(response.token);
-            this._isAuthenticated.next(true);
-            this.router.navigate(['/home']);
-        }
-      })
-    );
-  }
 }
