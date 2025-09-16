23f4620afe01ad1f384034f8d23be0ce5518b03d
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-16 02:24:05 -0600

    🔍️ Añadir seo a la página de inicio

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { SeoService } from '../../core/services/seo.service';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit{
+  private seoService = inject(SeoService);
 
+  ngOnInit(): void {
+    this.seoService.setAll({
+      title: 'Plataforma de autenticación y gestión de usuarios',
+      description: 'Bienvenido al módulo de autenticación y gestión de usuarios',
+      og: {
+        title: 'Plataforma de autenticación y gestión de usuarios',
+        description: 'Bienvenido al módulo de autenticación y gestión de usuarios',
+      }
+    });
+  }
 }
