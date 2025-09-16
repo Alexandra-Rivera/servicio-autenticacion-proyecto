981fe46675b9f75e3cab641df76c54182ac9eee1
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-15 23:23:31 -0600

    ♻️ Añadir una nueva ruta para inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
 import { Logout } from './features/auth/logout/logout';
 import { isLoggedInGuard, isLoggedOutGuard } from 'colibrihub-shared-components';
+import { Home } from './features/home/home';
 
 export const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: 'login' },
-    { path: 'login', component: Login, canMatch: [isLoggedOutGuard] },
-    { path: 'logout', component: Logout, canMatch: [isLoggedInGuard] },
-    { path: '**', redirectTo: ''}
+  { path: '', component: Home, pathMatch: 'full' },
+  { path: 'login', component: Login, canMatch: [isLoggedOutGuard] },
+  { path: 'logout', component: Logout, canMatch: [isLoggedInGuard] },
 ];
