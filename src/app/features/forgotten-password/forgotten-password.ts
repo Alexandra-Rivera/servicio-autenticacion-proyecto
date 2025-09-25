0f08d9f8c5914547dbd5f2b069adb5c43d139d7c
Alexandra Rivera <00162121@uca.edu.sv>
2025-09-25 03:06:02 -0600

    :lipstick: Agregando footer a componente de Olvidaste tu Contrasena

---
+++ src/app/features/forgotten-password/forgotten-password.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {LucideAngularModule, LucideMoveRight} from 'lucide-angular';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {Footer} from '../../shared/components/footer/footer';
+
+@Component({
+  selector: 'app-forgotten-password',
+  imports: [RouterLink, LucideAngularModule, FormsModule, ReactiveFormsModule, Footer],
+
+  templateUrl: './forgotten-password.html',
+  styleUrl: './forgotten-password.css'
+})
+export class ForgottenPassword {
+  readonly moveRight = LucideMoveRight;
+}
