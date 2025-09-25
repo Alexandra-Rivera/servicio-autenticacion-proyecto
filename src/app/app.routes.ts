4114e26c263a93f7a5b4febad7e27f688897cf81
Alexandra Rivera <00162121@uca.edu.sv>
2025-09-25 02:57:26 -0600

    :sparkles: Crear el componente de olvidaste tu contraseña

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Login } from './features/auth/login/login';
 import { Logout } from './features/auth/logout/logout';
 import { isLoggedInGuard, isLoggedOutGuard } from 'colibrihub-shared-components';
 import { Home } from './features/home/home';
+import {ForgottenPassword} from './features/forgotten-password/forgotten-password';
 
 export const routes: Routes = [
   { path: '', component: Home, pathMatch: 'full' },
   { path: 'login', component: Login, canMatch: [isLoggedOutGuard] },
   { path: 'logout', component: Logout, canMatch: [isLoggedOutGuard] },
+  { path: 'forgotten-password', component: ForgottenPassword},
   { path: '**', redirectTo: 'home' }
 ];
