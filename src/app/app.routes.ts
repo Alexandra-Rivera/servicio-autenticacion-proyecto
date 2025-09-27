dd0a87e810d178e02c518be44ed2f9e2a7126302
Alexandra Rivera <00162121@uca.edu.sv>
2025-09-26 22:54:42 -0600

    :sparkles: feat: Agregando ruta hacia componente de signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { Logout } from './features/auth/logout/logout';
 import { isLoggedInGuard, isLoggedOutGuard } from 'colibrihub-shared-components';
 import { Home } from './features/home/home';
 import {ForgottenPassword} from './features/forgotten-password/forgotten-password';
+import {Signup} from './features/auth/signup/signup';
 
 export const routes: Routes = [
   { path: '', component: Home, pathMatch: 'full' },
   { path: 'login', component: Login, canMatch: [isLoggedOutGuard] },
-  { path: 'logout', component: Logout, canMatch: [isLoggedOutGuard] },
+  { path: 'logout', component: Logout, canMatch: [isLoggedInGuard] },
   { path: 'forgotten-password', component: ForgottenPassword},
+  { path: 'signup', component: Signup},
   { path: '**', redirectTo: 'home' }
 ];
