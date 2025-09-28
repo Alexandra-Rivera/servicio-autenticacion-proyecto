9f967fcfc5a4075b88f914abf44576e08941d04f
Alexandra Rivera <00162121@uca.edu.sv>
2025-09-28 03:26:19 -0600

    :sparkles: feat: Agregar funcionalidad de recuperación de contraseña a forgotten-password.html y forgotten-password.ts

--- src/app/features/forgotten-password/forgotten-password.ts
+++ src/app/features/forgotten-password/forgotten-password.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import {LucideAngularModule, LucideMoveRight, LucideUser} from 'lucide-angular';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {Footer} from '../../shared/components/footer/footer';
+import {PasswordService} from '../../core/services/password.service';
+import {EmailDto} from '../../models/email-dto';
 
 @Component({
   selector: 'app-forgotten-password',
@@ -14,4 +16,49 @@ import {Footer} from '../../shared/components/footer/footer';
 export class ForgottenPassword {
   readonly moveRight = LucideMoveRight;
   readonly user = LucideUser;
+  protected email!: FormGroup;
+
+  constructor(
+    private passwordService: PasswordService,
+    private fb: FormBuilder,
+  ) {
+    this.email = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    })
+  }
+
+  passwordRecovery() {
+    if (this.email.valid) {
+      const emailDto: EmailDto = this.email.value;
+
+      this.passwordService.passwordRecovery(emailDto).pipe().subscribe({
+        next: response => {
+          console.log(response);
+        },
+        error: error => {
+          console.error("Error message:", error.error.message);
+        }
+      });
+    } else {
+      this.email.markAsTouched();
+    }
+  }
+
+  validateField (): boolean {
+    const field = this.email.get('email');
+
+    return field ? field.invalid && (field.dirty || field.touched) : false;
+  }
+
+  getErrorMessage(): string {
+    const field = this.email.get('email');
+    if (field?.hasError('required')) {
+      return 'Este campo es requerido';
+    }
+    if (field?.hasError('email')) {
+      return 'Ingresa una dirección válida de email';
+    }
+
+    return '';
+  }
 }
