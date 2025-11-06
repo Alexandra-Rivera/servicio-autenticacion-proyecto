59757b6588d80083bcf991d82718b15eb389cbca
Alexandra Rivera <00162121@uca.edu.sv>
2025-11-06 00:34:24 -0600

    :wastebasket: Eliminando código de ngOnDestroy()

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,9 +1,9 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavigationEnd, Router, RouterLink} from '@angular/router';
 import AOS from 'aos';
 import { SessionButton } from '../session-button/session-button';
 import {NgClass, NgOptimizedImage} from '@angular/common';
-import {filter, Subject, takeUntil} from 'rxjs';
+import {filter} from 'rxjs';
 import {LucideAngularModule, LucideX, Menu} from 'lucide-angular';
 import {LinkContent} from '../link-content/link-content';
 import {AppTitle} from '../app-title/app-title';
@@ -14,12 +14,11 @@ import {AppTitle} from '../app-title/app-title';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
-export class Navbar implements OnInit, OnDestroy {
+export class Navbar implements OnInit {
   //Icons
   readonly menu = Menu;
   readonly x = LucideX;
 
-  private destroy$ = new Subject<void>(); // Gestiona una subscripción
   currentRoute = "/";
   isMenuOpen = false;
   appTitle = "Colibrihub Systems";
@@ -38,16 +37,10 @@ export class Navbar implements OnInit, OnDestroy {
     // Suscribirse a los eventos del router
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd),
-      takeUntil(this.destroy$)
     ).subscribe((event: NavigationEnd) => {
       this.currentRoute = event.url;
       console.log(this.currentRoute);
     });
-  }
-
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
   }
 
   toggleMenu() {
