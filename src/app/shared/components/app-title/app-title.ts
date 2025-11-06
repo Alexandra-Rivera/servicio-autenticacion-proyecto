ccb03d4b1f15368a44e106d7b8a309d0351310e8
Alexandra Rivera <00162121@uca.edu.sv>
2025-11-05 22:38:42 -0600

    :lipstick: Modificando detalles en footer, navbar y componente app-title

--- src/app/shared/components/app-title/app-title.ts
+++ src/app/shared/components/app-title/app-title.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-title',
   imports: [
-    RouterLink
+    NgOptimizedImage
   ],
   templateUrl: './app-title.html',
   styleUrl: './app-title.css'
--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,25 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {NavigationEnd, Router, RouterLink} from '@angular/router';
 import AOS from 'aos';
 import { SessionButton } from '../session-button/session-button';
-import {NgClass} from '@angular/common';
-import {filter} from 'rxjs';
-import {LucideAngularModule, LucideHandCoins, LucideX, Menu} from 'lucide-angular';
+import {NgClass, NgOptimizedImage} from '@angular/common';
+import {filter, Subject, takeUntil} from 'rxjs';
+import {LucideAngularModule, LucideX, Menu} from 'lucide-angular';
 import {LinkContent} from '../link-content/link-content';
 import {AppTitle} from '../app-title/app-title';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, SessionButton, NgClass, LucideAngularModule, LinkContent, AppTitle],
+  imports: [RouterLink, SessionButton, NgClass, LucideAngularModule, LinkContent, AppTitle, NgOptimizedImage],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
-export class Navbar implements OnInit {
+export class Navbar implements OnInit, OnDestroy {
   //Icons
   readonly menu = Menu;
   readonly x = LucideX;
-  readonly handCoins = LucideHandCoins;
 
+  private destroy$ = new Subject<void>(); // Gestiona una subscripción
   currentRoute = "/";
   isMenuOpen = false;
   appTitle = "Colibrihub Systems";
@@ -28,6 +28,7 @@ export class Navbar implements OnInit {
     private router: Router,
   ) {
   }
+
   ngOnInit(): void {
     AOS.init({
       duration: 2000, // Set default duration or override with data attributes
@@ -36,11 +37,17 @@ export class Navbar implements OnInit {
 
     // Suscribirse a los eventos del router
     this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
+      filter(event => event instanceof NavigationEnd),
+      takeUntil(this.destroy$)
     ).subscribe((event: NavigationEnd) => {
-      // Verificar si la URL actual es la del "Home"
       this.currentRoute = event.url;
+      console.log(this.currentRoute);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   toggleMenu() {
