0723abaff17981f0edebffea6e2855e4794d9af7
Alexandra Rivera <00162121@uca.edu.sv>
2025-11-06 12:02:26 -0600

    :adhesive_bandage: Modificando titles para cada ruta

--- src/app/features/password-recovery/password-recovery.routes.ts
+++ src/app/features/password-recovery/password-recovery.routes.ts
@@ -5,8 +5,8 @@ import {ForgottenPassword} from './forgotten-password/forgotten-password';
 import {SuccessMessage} from './success-message/success-message';
 
 export const PasswordRecoveryRoutes: Routes = [
-  { path: '', component: ForgottenPassword },
-  { path: 'two-step-verification', component: PasswordCodeVerification },
-  { path: 'reset-password', component: ResetPassword },
-  { path: 'success', component: SuccessMessage },
+  { path: '', component: ForgottenPassword, title: 'Recuperación de contraseña' },
+  { path: 'two-step-verification', component: PasswordCodeVerification, title: 'Código de verificación' },
+  { path: 'reset-password', component: ResetPassword, title: 'Recuperación de contraseña' },
+  { path: 'success', component: SuccessMessage, title: 'Recuperación de contraseña' },
 ]
